0d7220f39601c0bc6c1beef4b57684442d5aefa3
Xander <87785817+XanderYZZ@users.noreply.github.com>
2026-07-19 11:50:54 -0700

    fix: resolve issues

--- src/main.py
+++ src/main.py
@@ -21,53 +21,40 @@ def _setup_data():
 
     X, y = mnist["data"], mnist["target"]
     y = y.astype(np.uint8)
-    some_digit = X.iloc[0]
+    some_digit = X.iloc[[0]]
 
     train_test_threshold = 60000 # training set is first 60K images, test set is last 10K images 
     X_train, X_test, y_train, y_test = X[:train_test_threshold], X[train_test_threshold:], y[:train_test_threshold], y[train_test_threshold:]
 
-    # binary classification (5 or not 5) that I did first off
-    """y_train_5 = (y_train == 5) 
-    y_test_5 = (y_test == 5)
-    sgd_clf = SGDClassifier(random_state=RANDOM_STATE_SEED)
-    sgd_clf.fit(X_train, y_train_5)
-    # Convert prediction sample into a DataFrame with the correct column names
-    sample_df = pd.DataFrame([some_digit], columns=X_train.columns)
-
-    # Predict without warnings
-    print(sgd_clf.predict(sample_df))
-
-    return (some_digit, X_train, y_train_5, sgd_clf)"""
-
     return (X_train, y_train, X_test, y_test, some_digit)
 
-def _plot_precision_recall_vs_threshold(y_train_5, y_scores):
-    precisions, recalls, thresholds = precision_recall_curve(y_train_5, y_scores)
+def _plot_precision_recall_vs_threshold(y_train, y_scores):
+    precisions, recalls, thresholds = precision_recall_curve(y_train, y_scores)
     plt.plot(thresholds, precisions[:-1], "b--", label="Precision")
     plt.plot(thresholds, recalls[:-1], "g-", label="Recall")
     plt.legend()
     plt.savefig("precision_recall_vs_threshold.png")
 
-def _cross_validate(some_digit, X_train, y_train_5, sgd_clf):
+def _cross_validate(some_digit, X_train, y_train, sgd_clf):
     skfolds = StratifiedKFold(n_splits=3, random_state=RANDOM_STATE_SEED, shuffle=True)
 
-    for train_index, test_index in skfolds.split(X_train, y_train_5):
+    for train_index, test_index in skfolds.split(X_train, y_train):
         clone_clf = clone(sgd_clf)
         X_train_folds = X_train.iloc[train_index]
-        y_train_folds = y_train_5.iloc[train_index]
+        y_train_folds = y_train.iloc[train_index]
         X_test_fold = X_train.iloc[test_index]
-        y_test_fold = y_train_5.iloc[test_index]
+        y_test_fold = y_train.iloc[test_index]
         clone_clf.fit(X_train_folds, y_train_folds)
         y_pred = clone_clf.predict(X_test_fold)
         n_correct = sum(y_pred == y_test_fold)
         print(n_correct / len(y_pred))
     
-    print(cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy"))
-    y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
-    print(confusion_matrix(y_train_5, y_train_pred))
-    print(precision_score(y_train_5, y_train_pred))
-    print(recall_score(y_train_5, y_train_pred))
-    print(f1_score(y_train_5, y_train_pred))
+    print(cross_val_score(sgd_clf, X_train, y_train, cv=3, scoring="accuracy"))
+    y_train_pred = cross_val_predict(sgd_clf, X_train, y_train, cv=3)
+    print(confusion_matrix(y_train, y_train_pred))
+    print(precision_score(y_train, y_train_pred))
+    print(recall_score(y_train, y_train_pred))
+    print(f1_score(y_train, y_train_pred))
 
 """
     The Receiver operating characteristic (ROC) curve is a common tool used with binary classifiers. 
@@ -91,19 +78,19 @@ def _plot_roc_curves(y_train, y_scores):
 def _run_svm_classifier(X_train, y_train, some_digit):
     svm_clf = SVC()
     svm_clf.fit(X_train, y_train)
-    print(svm_clf.predict([some_digit]))
+    print(svm_clf.predict(some_digit))
 
 def _run_ovr_classifier(X_train, y_train, some_digit):
     ovr_clf = OneVsRestClassifier(SVC())
     ovr_clf.fit(X_train, y_train)
-    print(ovr_clf.predict([some_digit]))
+    print(ovr_clf.predict(some_digit))
     print(len(ovr_clf.estimators_))
 
 def _run_sgd_classifier(X_train, y_train, some_digit):
-    sgd_clf = SGDClassifier()
+    sgd_clf = SGDClassifier(max_iter=2000, tol=1e-3)
     sgd_clf.fit(X_train, y_train)
-    print(sgd_clf.predict([some_digit]))
-    print(sgd_clf.decision_function([some_digit]))
+    print(sgd_clf.predict(some_digit))
+    print(sgd_clf.decision_function(some_digit))
     print(cross_val_score(sgd_clf, X_train, y_train, cv=3, scoring="accuracy"))
     scaler = StandardScaler()
     X_train_scaled = scaler.fit_transform(X_train.astype(np.float64))
@@ -143,7 +130,7 @@ def _run_multilabel_classification(X_train, y_train, some_digit):
     y_multilabel = np.c_[y_train_large, y_train_odd]
     knn_clf = KNeighborsClassifier()
     knn_clf.fit(X_train, y_multilabel)
-    print(knn_clf.predict([some_digit]))
+    print(knn_clf.predict(some_digit))
     y_train_knn_pred = cross_val_predict(knn_clf, X_train, y_multilabel, cv=3)
     print(f1_score(y_multilabel, y_train_knn_pred, average="macro"))
 
